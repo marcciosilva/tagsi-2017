36bdd770b597c68b5fc5cb9f5f95bf6f52959341
Marccio Silva <marccio.silva@fing.edu.uy>
2017-10-05 23:16:18 -0300

    Add main components, Bootstrap and jQuery.

--- bus-search/src/app/app.module.ts
+++ bus-search/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
